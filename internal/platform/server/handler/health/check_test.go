fc2f89ccf7c033c948fb9d65e96b2e55d625b5e6
Thefoovault <marta.ag.ma@gmail.com>
2024-05-18 11:16:02 +0200

    Added health check test

---
+++ internal/platform/server/handler/health/check_test.go
@@ -0,0 +1,29 @@
+package health
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_Check(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+	r.GET("/health", CheckHandler())
+
+	t.Run(`it returns 200`, func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/health", nil)
+		require.NoError(t, err)
+
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+}
